b27e18ce867bbfb17e99bda152dc7f27a3475696
Dario Ignacio <0943605360@aitec.edu.ec>
2025-07-13 17:55:01 -0500

    se quito la importacion de dio porque no se la utilizaba

--- frontend/lib/screens/auth/login_screen_simple.dart
+++ frontend/lib/screens/auth/login_screen_simple.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:dio/dio.dart';
 import '../../providers/auth_provider.dart';
 import '../../widgets/connection_status_widget.dart';
 
